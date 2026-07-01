d3753e27e75d528eae6665b1beb89893be90bd0c
Priyanka Pandit <priyankapandit0012gmail.com>
2026-07-01 16:54:33 +0530

    auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { loginUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
@@ -16,5 +17,10 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+    router.route("/login").post(loginUser)
+
+    //secured routes
+    router.route("/logout").post(verifyJWT,  logoutUser)
 
 export default router
