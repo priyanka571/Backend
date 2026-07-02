a193d74025af3a008e62eee4bc83f7a131fc31ac
Priyanka Pandit <priyankapandit0012gmail.com>
2026-07-02 16:25:43 +0530

    final

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -139,8 +139,8 @@ const loginUser = asyncHandler(async(req,res)=>{
 const logoutUser = asyncHandler(async(req, res)=>{
     await User.findByIdAndUpdate(
         req.user._id,{
-            $set:{
-                refreshToken:undefined
+             $unset: {
+                refreshToken: 1 // this removes the field from document
             }
         },{
             new:true
