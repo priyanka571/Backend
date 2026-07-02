e09f24a57cfa3f1eb20f1b65370b918bb6c5ce65
Priyanka Pandit <priyankapandit0012gmail.com>
2026-07-02 14:05:46 +0530

    Implement user authentication features: add refresh token functionality, change password, and user profile management

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,13 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, 
+         logoutUser,
+         loginUser, 
+         refreshAccessToken
+       } 
+from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router()
 
@@ -21,6 +27,7 @@ router.route("/register").post(
     router.route("/login").post(loginUser)
 
     //secured routes
-    router.route("/logout").post(verifyJWT,  logoutUser)
+    router.route("/logout").post(verifyJWT,logoutUser)
+    router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
